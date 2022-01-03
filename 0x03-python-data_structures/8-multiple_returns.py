d188cc436959233b8e89f572d0b78db1922f2110
EyaZidi19 <3805@holbertonschool.com>
2022-01-03 15:53:48 -0800

    update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is "" or not sentence:
-        return (0, None)
+    if sentence == "" or not sentence:
+        return ( None)
     return (len(sentence), sentence[0])
