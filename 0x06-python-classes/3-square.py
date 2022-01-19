d2df2f38cbb887f751823ba7db837deb2e541942
EyaZidi19 <3805@holbertonschool.com>
2022-01-19 09:13:59 -0800

    task3 correct PEP8

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""The class that returns the current square area of the square """
+"""The class that returns the current area of the square"""
 
 
 class Square:
-
-    """
-    Class Square that defines a square.
+    """Class Square that defines a square.
     Attributes:
-    size(int) - instance private: The size of a square
-    must be an integer greater than zero.
-    def area(self): - Public instance method: returns the 
-    current square area 
+        size(int) - instance private: The size of a square
+        must be an integer greater than zero.
     Errors:
-    TypeError - must be an integer
-    ValueError - must be greater than zero 
+        TypeError - must be an integer
+        ValueError - must be greater than zero
     """
 
+    """
+    Method to initialize an object or instance
+    and check if there are any kind of error.
+    """
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
@@ -23,7 +23,9 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-     
+    """
+    Method to return the area of the square.
+    """
     def area(self):
         res = self.__size * self.__size
         return res
