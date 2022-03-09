6f4d6aed463671d5d42b25e4cde33e19e9288051
EyaZidi19 <91125384+EyaZidi19@users.noreply.github.com>
2022-03-09 07:23:45 -0500

    script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- scrpt to List all privileges of the MySQL users user_0d_1 and user_0d_2
+-- GRANT is a cmd used to provide access or privileges on the DB objects to the users.
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
