9b6b253f306b2d1d8175e3b39679a3b90eeff217
ZidiEya <3805@holbertonschool.com>
2022-05-12 11:07:48 -0700

    fix issues

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-//script that prints a message depending of the number of arguments passed 
+// script that prints a message depending of the number of arguments
 const len = process.argv.length;
-
 if (len < 3) {
   console.log('No argument');
 } else if (len === 3) {
