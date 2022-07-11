f2692f036bee9a150c0b8916c989999a1d2f7302
ZidiEya <3805@holberton.com>
2022-07-11 20:23:46 +0000

    script that imports an array and computes a new array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,23 +1,7 @@
 #!/usr/bin/node
 // script that imports an array and computes a new array
-const movieID = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
-const request = require('request');
+const { list } = require('./100-data');
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error('error:', error);
-  } else {
-    const characterList = JSON.parse(body).characters;
-    for (const characterUrl of characterList) {
-      request(characterUrl, function (error, response, body) {
-        if (error) {
-          console.error('error:', error);
-        } else {
-          const peopleJson = JSON.parse(body);
-          console.log(peopleJson.name);
-        }
-      });
-    }
-  }
-});
+const newList = list.map((x, index) => x * index);
+console.log(list);
+console.log(newList);
