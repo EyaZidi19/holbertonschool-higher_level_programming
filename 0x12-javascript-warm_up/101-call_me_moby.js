26b810644cbb068e061786293d2e8919bc13db72
ZidiEya <3805@holberton.com>
2022-07-11 13:57:50 +0000

    function that executes x times a function.

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// func that executes x times a function
+// func that executes x times a fun
 function callMeMoby (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
